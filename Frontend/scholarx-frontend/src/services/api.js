f22c910a41912a73606e65c379a1fec5b2324a6a
Harsha <kjayaharsha8email@gmail.com>
2026-04-30 01:38:36 +0530

    connect frontend to live backend

--- Frontend/scholarx-frontend/src/services/api.js
+++ Frontend/scholarx-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://scholarx-backend-48t0.onrender.com"
 });
 
 // 🔥 attach user id automatically
